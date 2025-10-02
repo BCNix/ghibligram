4e73e7a27faba47fa6476eb8d7fb29358d06c5f0
BCNix <ilybevanz@gmail.com>
2025-10-01 20:39:59 -0700

    Remove uuid node module

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 import { whispersData } from './data.js'
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 
-// console.log(uuidv4())
+console.log(uuidv4())
 
 
 // whispersData.forEach(function(whisper){
 //     console.log(whisper.handle)
 // })
+
+
